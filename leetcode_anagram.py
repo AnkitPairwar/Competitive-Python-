05a712480040fc2be7f4eed186468549a1d9689d
ankit_pairwar <ankitdk3@gmail.com>
2020-04-08 03:10:21 +0530

    anagram

--- leetcode_anagram.py
+++ leetcode_anagram.py
@@ -1,17 +1,8 @@
-strs = ["eat","tea","tan","ate","nat","bat"]
-ans_ls , ind ,done = [[strs[0]]] , 0 , False
-for i in range(1,len(strs),1):
-    while ind != len(ans_ls)-1:
-        if len(strs[i] and ans_ls[ind]) == len(strs[i]):
-            ans_ls[ind].append(strs[i])
-            ind = 0
-            done = True
-            break
-        else:
-            print(i)
-            
-            ind+=1
-    if done == False:
-        ans_ls.append([strs[i]])
-print(ans_ls)       
-        
+from collections import defaultdict
+#strs = ["eat","tea","tan","ate","nat","bat"]
+ans_map ,ans_ls,coll = defaultdict(list) , list() ,list()
+for i in range(len(strs)): 
+    ans_map["".join(sorted(strs[i]))].append(strs[i])
+for i in ans_map.values():
+    coll.append(i)
+return coll
