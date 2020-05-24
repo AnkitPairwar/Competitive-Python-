e858e15330be9da1015b15bfc662a91132662578
ankit_pairwar <ankitdk3@gmail.com>
2020-05-25 03:21:10 +0530

    added tempelate file

---
+++ CP_may/tempelate.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <algorithm>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+int main() {
+    int test_cases;
+    cin >> test_cases;
+    
+    for(int i=0;i<test_cases;++i){
+
+    }
+
+    for(int i=0;i<test_cases;++i){
+        cout >>"\n"  >>;
+    }
+    
+    return 0;
+
+}
