84f433e9929ab6ad8b65f60e104f93abf28b5d74
ankit_pairwar <ankitdk3@gmail.com>
2020-07-17 19:08:45 +0530

    new cont

---
+++ gfg/subarr_greater_than_k.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <set>
+#define ll long long
+#define test int t;cin>> t;while(t--)
+#define Y "Yes"
+#define N "No"
+#define pb push_back
+#define pp pop_back
+#define f(i,n) for(int i=0;i<n;++i)
+#define fd(i,n) for(int i=n-1;i>=0;--i)
+//#include <cstudio>
+
+using namespace std;
+int main() {
+    int ar[] = {1, 4, 45, 6, 0, 19};
+    int k = 51, min_len=INT_MAX
+     ,n=sizeof(ar)/sizeof(int),sum=0,ans=-1;
+    sort(ar ,ar+n);
+    fd(i,n)
+    {
+        
+    }
+    cout << ans;
+
+    return 0;
+
+}
